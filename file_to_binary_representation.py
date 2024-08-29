969c818da9a234c953c30635db03790e910e3836
Laxan3000 <58862419+Laxan3000@users.noreply.github.com>
2024-08-29 16:00:25 +0200

    Update file_to_binary_representation.py

--- file_to_binary_representation.py
+++ file_to_binary_representation.py
@@ -5,10 +5,15 @@ from PIL.PngImagePlugin import PngInfo
 from math import ceil
 import numpy as np
 
-IMAGE_MODE: str = "L" # Grayscale
 IMAGE_FORMAT: str = "png"
 METADATA_NAME: str = "OGFileSize"
 FILE_TYPE: list[tuple[str, str]] = [("Binary Rapresentation", f"*.{IMAGE_FORMAT}")]
+COMPATIBLE_MODES: tuple[tuple[str, str]] = (
+    ("L", "grayscale"),
+    ("LA", "+ transparency"),
+    ("RGB", "true color"),
+    ("RGBA", "+ transparency")
+)
 
 
 def get_file(force_format: bool = False) -> str:
@@ -18,6 +23,21 @@ def get_file(force_format: bool = False) -> str:
         function.keywords["filetypes"] = FILE_TYPE
     
     return function()
+
+
+def get_mode() -> str:
+    while True:
+        mode: str = input(
+            "Please select an image mode from this list:\n" +
+            '\n'.join(
+                f"{i} - {name} ({desc})"
+                for i, (name, desc) in enumerate(COMPATIBLE_MODES)
+            ) +
+            "\nSelect your answer [2]: ",
+        ) or '2'
+        
+        if '0' <= mode <= f'{len(COMPATIBLE_MODES) - 1}':
+            return COMPATIBLE_MODES[int(mode)][0]
 
 
 def get_compression() -> int:
@@ -34,6 +54,9 @@ def to_image() -> bool:
     if not (file_name := get_file()):
         return False
     
+    mode: str = get_mode()
+    bytesnum: int = sum(1 for c in mode if c.isupper())
+    
     # Read the image from a numpy array
     with Image.fromarray(
         # Pad a coming array at its tail, adding 0 for each missed pixel
@@ -45,27 +68,30 @@ def to_image() -> bool:
             # - (0, ...): 
             #   0 is how much padding has to be added at the head of the array
             #   ... is how much padding has to be added at the tail of the array
-            (0, (side := ceil((size := len(a)) ** 0.5)) ** 2 - size),
-            
+            (0, (side := ceil(ceil((size := len(a)) / bytesnum) ** 0.5)) ** 2 * bytesnum - size),
+        
             # "Constant" means it's keeping the upcoming value the same
             'constant',
             
             # The value to pad the array with
             constant_values= 0
             
-        # reshape the array so to make a square
-        ).reshape(side, side),
-        IMAGE_MODE
+        # Reshape the array so to make a square
+        ).reshape(side, side, bytesnum),
+        mode
     ) as image:
         # Prepare metadata to add to the image
         metadata: PngInfo = PngInfo()
         metadata.add_text(METADATA_NAME, str(size))
         
-        save_name: str = asksaveasfilename(
-            defaultextension= IMAGE_FORMAT,
-            filetypes= FILE_TYPE,
-            title= "What name do you want to give to your rapresentation?"
-        )
+        if not (
+            save_name := asksaveasfilename(
+                defaultextension= IMAGE_FORMAT,
+                filetypes= FILE_TYPE,
+                title= "What name do you want to give to your rapresentation?"
+            )
+        ):
+            return False
         
         # Save the image as png
         image.save(
@@ -79,14 +105,15 @@ def to_image() -> bool:
         
 
 def from_image() -> bool:
-    if not (file_name := get_file(True)):
+    if not (file_name := get_file(True)) \
+    or not (
+        # Asks the user the name to save the new file
+        save_name := asksaveasfilename(
+            filetypes= [("Text Files", "*.txt"), ("All Files", "*.*")],
+            title= "How do you want to save the file as?"
+        )
+    ):
         return False
-    
-    # Asks the user the name to save the new file
-    save_name: str = asksaveasfilename(
-        filetypes= [("Text Files", "*.txt"), ("All Files", "*.*")],
-        title= "How do you want to save the file as?"
-    )
 
     while True:
         # Open the image, then the hard part with numpy comes..
